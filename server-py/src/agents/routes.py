ea7e36333bb656b54768e0cf6bf55c009265bccc
Sean Ippolito <ippolitosean@gmail.com>
2025-02-06 22:42:56 -0800

    update text (#10)
    * updated README to better assist getting started with the server-py directory (#9)
    
    * update text

--- server-py/src/agents/routes.py
+++ server-py/src/agents/routes.py
@@ -3,7 +3,7 @@ from agents import agents
 from flask import jsonify
 from datetime import datetime
 
-@agents.route("/agent_one")
+@agents.route("/agent_test")
 def get_agent_response(temp):
     
 
